b56e62278a3f315429b8db6a486eba75dacb5309
Gou Faan <goufaan@gmail.com>
2019-07-25 20:38:56 +0800

    feat: add assignment dao mapper & entity

---
+++ web/src/main/java/com/sacc/ComprehensiveSystem/modules/assignment/controller/AssignmentController.java
@@ -0,0 +1,10 @@
+package com.sacc.ComprehensiveSystem.modules.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/assignment")
+public class AssignmentController {
+    
+}
